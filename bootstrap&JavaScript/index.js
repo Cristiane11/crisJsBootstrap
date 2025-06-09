eb7ce4f782d6f2a0f92d11beb131bacc817f16cd
Cristiane Abreu <bjwmcd@yahoo.com>
2025-06-08 22:12:18 -0400

    update

--- bootstrap&JavaScript/index.js
+++ bootstrap&JavaScript/index.js
@@ -6,7 +6,7 @@ let skillsUpdate = document.getElementById("skills");
 const continueUpSkills = skills.map((skill)=>{
         return  skill
 });
-skillsUpdate.innerHTML = continueUpSkills.slice(0,8);
+skillsUpdate.innerHTML = continueUpSkills.slice(0,8).join(' ');
 
 const projects=[
     {
@@ -39,13 +39,45 @@ const projects=[
     title:"Worpdress Website",
     projectSkills:"js, html, css,react",
     description:"wirtshoooiosssoosososodosof"
-
     }
 ];
 let ProjectsFront = document.getElementById("projectsSample");
-const proj = projects.map((project)=>{
-   return project
 
-})
-ProjectsFront.innerHTML =proj.slice(0,4);
+
+
+
+
+const cards = projects.map(project => {
+
+  return `
+
+    <div class="project-card p-5 col-md-3">
+
+      <img   
+
+        src="${project.img}"        
+
+        alt="${project.title}"        
+
+        class="project-card__image card-img-top"
+
+      />
+
+      <h3 class="project-card__title card-title">${project.title}</h3>
+
+      <p class="project-card__desc card-body border">${project.description}</p>
+
+      <p class="project-card__skills card-text">
+
+        ${project.projectSkills}
+
+      </p>
+
+    </div>
+
+  `;
+
+});
+ProjectsFront.innerHTML = cards.slice(0,4).join(' ');
+
 
